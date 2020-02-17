c7f4efd9c5e979abb9351beb5bd8d674bbe2c8bc
PrankurBishnoi <prankur.bishnoi@tothenew.com>
2020-02-17 20:35:45 +0530

    5

--- Java Assignment 1/src/CommonInArrays.java
+++ Java Assignment 1/src/CommonInArrays.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class CommonInArrays
 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int Array1[] = {24,547,756,23,43,6,87,54};
+        int Array2[] = {234,532,547,23,5,6,254,45};
+        for (int i : Array1)
+            for (int j : Array2)
+            {
+                if ( i == j )
+                {
+                    System.out.println(i);
+                }
+            }
 
+    }
 }
