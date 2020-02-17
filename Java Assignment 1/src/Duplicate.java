2d1c6af270cb8d0d8a39663e8a730e867e62c527
PrankurBishnoi <prankur.bishnoi@tothenew.com>
2020-02-17 20:15:42 +0530

    1,2,4

---
+++ Java Assignment 1/src/Duplicate.java
@@ -0,0 +1,30 @@
+import java.sql.SQLOutput;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+import static java.util.Arrays.*;
+
+public class Duplicate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String");
+        String Strings = sc.nextLine();
+        String[] arrOfStrings = Strings.split(" ");
+        Arrays.sort(arrOfStrings);
+        int c=1;
+        for (int i=0;i<arrOfStrings.length-1;i++)
+        {
+            if (arrOfStrings[i].equalsIgnoreCase(arrOfStrings[i+1]))
+            {
+                c++;
+            }
+            else
+            {
+                System.out.println("Occurence of "+arrOfStrings[i]+" is: "+c);
+                c=1;
+            }
+        }
+
+    }
+}
