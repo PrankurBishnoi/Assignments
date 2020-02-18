a95c7fa3a3b7fdcb5742044a9d3473406cab3ad7
PrankurBishnoi <prankur.bishnoi@tothenew.com>
2020-02-18 16:57:03 +0530

    Complete

--- CharDuplicate.java
+++ CharDuplicate.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 import java.lang.*;
-public class CharDuplicate
-{
-    public static void main(String[] args)
+    public class CharDuplicate
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a String");
-        String Strings = sc.nextLine();
-        int Length1 = Strings.length();
-        System.out.println("Enter the Character to be counted");
-        String characters = sc.next();
-        int Length2 = Strings.replace(characters,null).length();
-        System.out.println("Number of Occurance of '"+characters+"' is:  "+(Length1-Length2));
+        public static void main(String[] args)
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a String");
+            String Strings = sc.nextLine();
+            int Length1 = Strings.length();
+            System.out.println("Enter the Character to be counted");
+            String characters = sc.next();
+            int Length2 = Strings.replace(characters,"").length();
+            System.out.println("Number of Occurance of '"+characters+"' is:  "+(Length1-Length2));
+        }
     }
-}
+
+
