f1b26d38768b3be02b772561355beb5f66ec96b4
PrankurBishnoi <prankur.bishnoi@tothenew.com>
2020-02-19 22:52:00 +0530

    Ans 2

--- ThreadRunners.java
+++ ThreadRunners.java
@@ -1,43 +1,14 @@
-//public class ThreadRunners extends Thread
-//{
-//    public void run()
-//    {
-//        for (int i=0; i<5; i++)
-//        {
-//            System.out.println("Hello: "+ i);
-//
-//            try {
-//                Thread.sleep(100);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//}
-//class Mainss
-//{
-//    public static void main(String[] args)
-//    {
-//        ThreadRunners runner1 = new ThreadRunners();
-//        runner1.start();
-//
-//        ThreadRunners runner2 = new ThreadRunners();
-//        runner2.start();
-//    }
-//}
+//    Thread Class
 
-
-
-class ThreadRunners implements Runnable
+ public class ThreadRunners extends Thread
 {
-    @Override
     public void run()
     {
-        for (int i=0; i<5;i++)
+        for (int i=0; i<5; i++)
         {
-            System.out.println("Hello: "+i);
-            try
-            {
+            System.out.println("Hello: "+ i);
+
+            try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -45,12 +16,44 @@ class ThreadRunners implements Runnable
         }
     }
 }
-
 class Mainss
 {
     public static void main(String[] args)
     {
-        Thread Thread1 = new Thread(new ThreadRunners());
-        Thread1.start();
+        ThreadRunners runner1 = new ThreadRunners();
+        runner1.start();
+
+        ThreadRunners runner2 = new ThreadRunners();
+        runner2.start();
     }
 }
+
+
+// Runnable Interface
+
+//class ThreadRunners implements Runnable
+//{
+//    @Override
+//    public void run()
+//    {
+//        for (int i=0; i<5;i++)
+//        {
+//            System.out.println("Hello: "+i);
+//            try
+//            {
+//                Thread.sleep(100);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//}
+//
+//class Mainss
+//{
+//    public static void main(String[] args)
+//    {
+//        Thread Thread1 = new Thread(new ThreadRunners());
+//        Thread1.start();
+//    }
+//}
