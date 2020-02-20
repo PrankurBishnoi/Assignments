aa03c1d5665cff0d251d95a889b4c5a63d6e1be2
PrankurBishnoi <prankur.bishnoi@tothenew.com>
2020-02-20 16:34:06 +0530

    8

---
+++ Semaphores.java
@@ -0,0 +1,79 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class Semaphores {
+
+    private static Semaphores instance = new Semaphores();
+
+    private Semaphore sem = new Semaphore(10, true);
+
+    private int connections = 0;
+
+    private Semaphores() {
+
+    }
+
+    public static Semaphores getInstance() {
+        return instance;
+    }
+
+    public void connect() {
+        try {
+            sem.acquire();
+        } catch (InterruptedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        try {
+            doConnect();
+        } finally {
+
+            sem.release();
+        }
+    }
+
+    public void doConnect() {
+
+        synchronized (this) {
+            connections++;
+            System.out.println("Current connections: " + connections);
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        synchronized (this) {
+            connections--;
+        }
+
+    }
+}
+
+
+class Appss {
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for(int i=0; i < 200; i++) {
+            executor.submit(new Runnable() {
+                public void run() {
+                    Semaphores.getInstance().connect();
+                }
+            });
+        }
+
+        executor.shutdown();
+
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+
+}
